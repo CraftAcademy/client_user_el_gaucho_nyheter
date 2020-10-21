b4efbf35614baa0a585094a02c970b047663c423
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-21 07:49:20 +0200

    Refactoring feature spec for 'visitor can register'

--- cypress/integration/VisitorCanRegisterToBecomeAUser.feature.js
+++ cypress/integration/VisitorCanRegisterToBecomeAUser.feature.js
@@ -1,39 +1,48 @@
 describe("User can buy a subscription", () => {
   beforeEach(() => {
+    cy.server();
     cy.route({
       method: "POST",
-      url: "http://localhost:3000/api/v1/subscriptions",
+      url: "http://localhost:3000/api/v1/auth",
       response: "fixture:registration_success.json",
     });
+    cy.visit("/");
   });
 
   context("successfully", () => {
-    cy.get('[data-cy="register"]').click();
-    cy.get("[data-cy='register-form']").within(() => {
-      cy.get("[data-cy='email']").type("user123@mail.com");
-      cy.get("[data-cy='password']").type("password");
-      cy.get("[data-cy='submit']").click();
+    it("by registrating with valid credentials", () => {
+      cy.get('[data-cy="register"]').click();
+      cy.get("[data-cy='register-form']").within(() => {
+        cy.get("[data-cy='email']").type("user123@mail.com");
+        cy.get("[data-cy='password']").type("password");
+        cy.get("#password-confirmation").type("password")
+        cy.get("[data-cy='submit']").click();
+      });
+      cy.get("[data-cy=message]").contains(
+        "Registration successful, now you have access to El-gaucho mobile app. Visit your appstore!"
+      );
     });
-    cy.get("[data-cy=message]").contains(
-      "Registration successful, now you have access to El-gaucho mobile app. Visit your appstore!"
-    );
   });
 
   context("with invalid email", () => {
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/v1/auth/sign_in",
-      status: "422",
-      response: {
-        response: "fixture:registration_unsuccess.json"
-      },
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/auth/sign_in",
+        response: "fixture:registration_unsuccess.json",
+      });
     });
-    cy.get('[data-cy="register"]').click();
-    cy.get("[data-cy='register-form']").within(() => {
-      cy.get("[data-cy='email']").type("usermail.com");
-      cy.get("[data-cy='password']").type("password");
-      cy.get("[data-cy='submit']").click();
+
+    it("by registrating with valid credentials", () => {
+      cy.get('[data-cy="register"]').click();
+      cy.get("[data-cy='register-form']").within(() => {
+        cy.get("[data-cy='email']").type("usermail.com");
+        cy.get("[data-cy='password']").type("password");
+        cy.get("#password-confirmation").type("password")
+        cy.get("[data-cy='submit']").click();
+      });
+      cy.get("#message").should("contain", "Email must be valid");
     });
-    cy.get("#message").should("contain", "Email must be valid");
   });
 });
